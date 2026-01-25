e5a5666e97e6982179fcbee2ce4cd74c245cfaa9
matheussricardoo <matheusri1@hotmail.com>
2026-01-25 02:46:13 -0300

    feat(echo): Adding logic for double quotes.

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,8 @@ fn parse_input(input: &str) -> Vec<String> {
     let mut args = Vec::new();
     let mut current_arg = String::new();
     let mut in_single_quote = false;
+    let mut in_double_quote = false;
+    let mut escaped = false;
 
     for c in input.chars() {
         if in_single_quote {
@@ -37,9 +39,22 @@ fn parse_input(input: &str) -> Vec<String> {
             } else {
                 current_arg.push(c);
             }
+        } else if in_double_quote {
+            if escaped {
+                current_arg.push(c);
+                escaped = false
+            } else if c == '\\' {
+                escaped = true;
+            } else if c == '"' {
+                in_double_quote = false
+            } else {
+                current_arg.push(c);
+            }
         } else {
             if c == '\'' {
                 in_single_quote = true;
+            } else if c == '"' {
+                in_double_quote = true;
             } else if c.is_whitespace() {
                 if !current_arg.is_empty() {
                     args.push(current_arg);
