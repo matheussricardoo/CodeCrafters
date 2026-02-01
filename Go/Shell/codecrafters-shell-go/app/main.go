e8edd0e334e2090f97d99a662e4f69a417afe8f0
matheussricardoo <matheusri1@hotmail.com>
2026-01-31 23:42:45 -0300

    Starting the shell project in Go

---
+++ Go/Shell/codecrafters-shell-go/app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
+var _ = fmt.Print
+
+func main() {
+	fmt.Print("$ ")
+}
